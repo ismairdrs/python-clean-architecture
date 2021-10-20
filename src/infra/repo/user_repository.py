34b50b83161531746679e0554f02fec33f21e9ed
ismairdrs <ismairdrs@outlook.com>
2021-10-20 01:03:49 -0300

    feat: implementing select_user in UserRepository

--- src/infra/repo/user_repository.py
+++ src/infra/repo/user_repository.py
@@ -1,3 +1,4 @@
+from typing import List
 from src.infra.config import DBConnectionHandler
 from src.infra.entities import Users as UsersModel
 from src.domain.models import Users
@@ -22,5 +23,41 @@ class UserRepository:
                 return Users(
                     id=new_user.id, name=new_user.name, password=new_user.password
                 )
+            finally:
+                db_connection.session.close()
+
+    @classmethod
+    def select_user(cls, user_id: int = None, name: str = None) -> List[Users]:
+        """select data in user entity by id and or name"""
+
+        with DBConnectionHandler() as db_connection:
+            try:
+                query_data = None
+                if user_id and not name:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(id=user_id)
+                        .one()
+                    )
+                    query_data = [data]
+                elif not user_id and name:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(name=name)
+                        .one()
+                    )
+                    query_data = [data]
+                elif all([user_id, name]):
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(id=user_id, name=name)
+                        .one()
+                    )
+                    query_data = [data]
+            except:
+                db_connection.session.rollback()
+                raise
+            else:
+                return query_data
             finally:
                 db_connection.session.close()
