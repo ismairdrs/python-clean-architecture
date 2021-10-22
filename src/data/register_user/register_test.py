dc5de18b7097f61d975851276882b0ba00c6ed39
ismairdrs <ismairdrs@outlook.com>
2021-10-22 10:34:21 -0300

    test: implements test_register for test RegisterUser

---
+++ src/data/register_user/register_test.py
@@ -0,0 +1,42 @@
+from faker import Faker
+from src.infra.test import UserRepositorySpy
+from .register import RegisterUser
+
+faker = Faker()
+
+
+def test_register():
+    """test registry method"""
+
+    user_repo = UserRepositorySpy()
+    register_user = RegisterUser(user_repo)
+
+    attributes = {"name": faker.name(), "password": faker.name()}
+
+    response = register_user.register(
+        attributes["name"], password=attributes["password"]
+    )
+
+    assert user_repo.insert_user_params["name"] == attributes["name"]
+    assert user_repo.insert_user_params["password"] == attributes["password"]
+
+    assert response["Success"] is True
+    assert response["Data"]
+
+
+def test_register_fail():
+    """test registry method in fail"""
+
+    user_repo = UserRepositorySpy()
+    register_user = RegisterUser(user_repo)
+
+    attributes = {"name": faker.random_number(digits=5), "password": faker.name()}
+
+    response = register_user.register(
+        attributes["name"], password=attributes["password"]
+    )
+
+    assert user_repo.insert_user_params == {}
+
+    assert response["Success"] is False
+    assert response["Data"] is None
