c80787d106790ccc6da7946f951783a55b9589ba
ismairdrs <ismairdrs@outlook.com>
2021-10-27 15:40:16 -0300

    refactor: implementing heritage RegisterUserController

--- src/presenters/controllers/register_pet_controller.py
+++ src/presenters/controllers/register_pet_controller.py
@@ -9,13 +9,13 @@ class RegisterPetController(RouteInterface):
     def __init__(self, register_pet_use_case: Type[RegisterPet]):
         self.register_pet_use_case = register_pet_use_case
 
-    def route(self, request: HttpRequest) -> HttpResponse:
+    def route(self, http_request: HttpRequest) -> HttpResponse:
         required_fields = ["name", "specie", "user_information"]
         for field in required_fields:
-            if field not in request.body.keys():
+            if field not in http_request.body.keys():
                 return FactoryHttpError(HttpErrors.error_400()).run()
 
-        user_information_params = request.body.get("user_information")
+        user_information_params = http_request.body.get("user_information")
         if not any(
             [
                 "user_id" in user_information_params,
@@ -24,10 +24,10 @@ class RegisterPetController(RouteInterface):
         ):
             return FactoryHttpError(HttpErrors.error_400()).run()
 
-        name = request.body.get("name")
-        specie = request.body.get("specie")
-        user_information = request.body.get("user_information")
-        age = request.body.get("age", None)
+        name = http_request.body.get("name")
+        specie = http_request.body.get("specie")
+        user_information = http_request.body.get("user_information")
+        age = http_request.body.get("age", None)
 
         response = self.register_pet_use_case.registry(
             name=name, specie=specie, user_information=user_information, age=age
