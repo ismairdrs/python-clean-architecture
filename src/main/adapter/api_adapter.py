90103663c7ba483ee62cf858247624ba42c4d8ba
ismairdrs <ismairdrs@outlook.com>
2021-10-28 14:11:53 -0300

    feat: call FindValidations for evaluate query string params

--- src/main/adapter/api_adapter.py
+++ src/main/adapter/api_adapter.py
@@ -1,12 +1,21 @@
 from typing import Type
 from sqlalchemy.exc import IntegrityError
 from src.main.interface import RouteInterface as Route
+from src.main.validations import FindValidations
 from src.presenters.helpers import HttpRequest
 from src.presenters.erros import HttpErrors, FactoryHttpError
 
 
 def flask_adapter(request: any, api_route: Type[Route]) -> any:
-    http_request = HttpRequest(body=request.json)
+    if request.method == "POST":
+        http_request = HttpRequest(body=request.json)
+
+    if request.method == "GET":
+        query_string_params = FindValidations(request=request).validate()
+        http_request = HttpRequest(
+            header=request.headers, body=request.json, query=query_string_params
+        )
+
     try:
         http_response = api_route.route(http_request)
     except IntegrityError:
