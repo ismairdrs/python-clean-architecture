3cdc32bb465d61d1d24699c516ebacfaa3aa3369
ismairdrs <ismairdrs@outlook.com>
2021-10-28 11:59:24 -0300

    feat: implementing find_user_composer

---
+++ src/main/composer/find_user_composite.py
@@ -0,0 +1,9 @@
+from src.presenters.controllers import FindUserController
+from src.data.find_user import FindUser
+from src.infra.repo.user_repository import UserRepository
+
+
+def find_user_composer():
+    repository = UserRepository()
+    find_user = FindUser(repository)
+    return FindUserController(find_user)
