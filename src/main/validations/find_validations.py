09f90ac272544e5922fed23392a7bd464d4aa3d3
ismairdrs <ismairdrs@outlook.com>
2021-10-28 14:11:17 -0300

    refactor: include try except in method validate

--- src/main/validations/find_validations.py
+++ src/main/validations/find_validations.py
@@ -1,4 +1,5 @@
 from src.main.interface import Validation
+from src.presenters.erros import HttpErrors, FactoryHttpError
 
 
 class FindValidations(Validation):
@@ -6,10 +7,14 @@ class FindValidations(Validation):
         self.request = request
 
     def validate(self):
-        if self.request.method == "GET":
-            query_string_params = self.request.args.to_dict()
-            if "user_id" in query_string_params:
-                query_string_params["user_id"] = int(query_string_params["user_id"])
-            if "pet_id" in query_string_params:
-                query_string_params["pet_id"] = int(query_string_params["pet_id"])
-        return query_string_params
+        try:
+            if self.request.method == "GET":
+                query_string_params = self.request.args.to_dict()
+                if "user_id" in query_string_params:
+                    query_string_params["user_id"] = int(query_string_params["user_id"])
+                if "pet_id" in query_string_params:
+                    query_string_params["pet_id"] = int(query_string_params["pet_id"])
+            return query_string_params
+        except Exception as exc:  # pylint: disable=broad-except
+            print(exc)
+            return FactoryHttpError(HttpErrors.error_400()).run()
